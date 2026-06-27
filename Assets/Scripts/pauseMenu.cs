88df0c1c7fe4cc62327ef5b88d53036e9ee24ed9
Dakspieth <jrb.games09@gmail.com>
2026-06-26 20:55:02 -0400

    added pause keybind

--- Assets/Scripts/pauseMenu.cs
+++ Assets/Scripts/pauseMenu.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
 public class pauseMenu : MonoBehaviour
 {
     public GameObject PauseMenu;
-
+    public InputAction pauseControl;
+    bool read = true;
     void Start()
     {
         Time.timeScale = 1;
     }
+    void Update()
+    {
+        if(pauseControl.ReadValue<float>() == 1 && read)
+        {
+            PauseButtonPressed();
+            read = false;
+        } else if(pauseControl.ReadValue<float>() == 0)
+        {
+            read = true;
+        }
+    }
+
+    void OnEnable()
+    {
+        pauseControl.Enable();
+    }
+    void OnDisable()
+    {
+        pauseControl.Disable();
+    }
+
     public void PauseButtonPressed()
     {
         PauseMenu.SetActive(!PauseMenu.activeSelf);
